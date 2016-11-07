b041d5c3ee6bcc227742f3d3ad8c81a2afc4414f
timtrippel <timtrippel@github.com>
2016-11-07 10:57:29 -0500

    changed return value to input buffer value

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -29,9 +29,9 @@ sgx_status_t genKey(sgx_ec256_public_t* pub) {
   // sgx_ec256_public_t     *p_public_key  = NULL; 
 
   // Open ECC256 Context
-  if(SGXAPI sgx_ecc256_open_context(p_ecc_handle) != SGX_SUCCESS){
-    return SGX_ERROR_UNEXPECTED;
-  }
+  // if(SGXAPI sgx_ecc256_open_context(p_ecc_handle) != SGX_SUCCESS){
+  //   return SGX_ERROR_UNEXPECTED;
+  // }
 
   // Generate ECC256 Key Pair with ECC256 Context
   // if(SGXAPI sgx_ecc256_create_key_pair(p_private_key, pub, p_ecc_handle) != SGX_SUCCESS){

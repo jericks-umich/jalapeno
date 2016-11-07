a215948a76ec661f6119c3618d92b85c795907be
timtrippel <timtrippel@github.com>
2016-11-07 11:49:48 -0500

    changed return value to input buffer value

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -12,42 +12,41 @@
 ////////////
 
 int say_hello() {
-  char str[] = "Hello SGX!";
-  int retval;
-  ocall_prints(&retval, str);
-  return SGX_SUCCESS;
+	char str[] = "Hello SGX!";
+	int retval;
+	ocall_prints(&retval, str);
+	return SGX_SUCCESS;
 }
 
 // pub is for passing back the new public key
 sgx_status_t genKey(sgx_ec256_public_t* pub) {
-  char str[]          = "Called genKey()";
-  int retval          = 0;
-  sgx_status_t status = SGX_SUCCESS;
+	// char str[]          = "Called genKey()";
+	// int retval          = 0;
+	sgx_status_t status = SGX_SUCCESS;
 
-  sgx_ecc_state_handle_t p_ecc_handle;
-  sgx_ec256_private_t    p_private_key;
-  // sgx_ec256_spublic_t     *p_public_key  = NULL; 
+	sgx_ecc_state_handle_t p_ecc_handle;
+	sgx_ec256_private_t    p_private_key;
+	sgx_ec256_spublic_t    p_public_key; 
 
-  // Open ECC256 ContextSGXAPI 
-  if(SGXAPI sgx_ecc256_open_context(&p_ecc_handle) != SGX_SUCCESS){
-    return SGX_ERROR_UNEXPECTED;
-  }
+	// Open ECC256 Context
+	if(SGXAPI sgx_ecc256_open_context(&p_ecc_handle) != SGX_SUCCESS){
+		return SGX_ERROR_UNEXPECTED;
+	}
 
-  // Generate ECC256 Key Pair with ECC256 Context
-  if(SGXAPI sgx_ecc256_create_key_pair(&p_private_key, pub, p_ecc_handle) != SGX_SUCCESS){
-    return SGX_ERROR_UNEXPECTED;
-  }
+	// Generate ECC256 Key Pair with ECC256 Context
+	if(SGXAPI sgx_ecc256_create_key_pair(&p_private_key, &p_public_key, p_ecc_handle) != SGX_SUCCESS){
+		return SGX_ERROR_UNEXPECTED;
+	}
 
-  // Close ECC256 Context
-  if(SGXAPI sgx_ecc256_close_context(p_ecc_handle) != SGX_SUCCESS){
-    return SGX_ERROR_UNEXPECTED; 
-  }
+	// Close ECC256 Context
+	if(SGXAPI sgx_ecc256_close_context(p_ecc_handle) != SGX_SUCCESS){
+		return SGX_ERROR_UNEXPECTED; 
+	}
 
-  // Copy memory of public key
-  // memcpy(p_public_key, pub, sizeof(sgx_ec256_public_t));
-  ocall_prints(&retval, str);
-  // ocall_print_key(&retval, str);
-  return SGX_SUCCESS;
+	// Copy memory of public key
+	memcpy(p_public_key, pub, sizeof(sgx_ec256_public_t));
+	// ocall_prints(&retval, str);
+	return SGX_SUCCESS;
 }
 
 // pub is input for finding the keypair to delete them
@@ -84,8 +83,6 @@ sgx_status_t sign(const uint8_t* plaintext, uint32_t len, sgx_ec256_public_t* pu
   signature->x[0] = 3;
   return SGX_SUCCESS;
 }
-
-
 
 //////////////////
 // PUBLIC DEBUG //
